4f527196dd7a2feaa04a3522a9cb1a44a47d174b
COOL-CAL <78495826+COOL-CAL@users.noreply.github.com>
2022-04-27 18:59:12 +0900

    Update list.php

--- list.php
+++ list.php
@@ -1,11 +1,26 @@
 <?php
+    include_once "db/db_board.php";
+
     session_start();
     $nm = "";
-    if(isset($_SESSION["login_user"]))
-    {
+    $page = $_GET["page"];
+    if(!$page) {
+        $page = 1;
+    } else {
+        $page = intval($page);
+    }
+    print "page : " . $page;
+    if(isset($_SESSION["login_user"])) {
         $login_user = $_SESSION["login_user"];
         $nm = $login_user["nm"];
     }
+    $row_count = 20;
+    $param = [
+        "row_count" => $row_count,
+        "start_idx" => ($page - 1) * $row_count
+    ];
+    $paging_count = sel_paging_count($param);
+    $list = sel_board_list($param);
 ?>
 
 <!DOCTYPE html>
@@ -23,25 +38,45 @@
             <?=isset($_SESSION["login_user"]) ? "<div>" . "Welcome,". $nm . "</div>" : ""?>
             <div>
                 <a href="list.php">LIST</a>
-                <a href="write.php">WRITE</a>
-                <?=isset($_SESSION["login_user"]) ? "<a href='logout.php'>LOGOUT</a>" : "<a href='login.php'>LOGIN</a>"?>
+                <?php if(isset($_SESSION["login_user"])) { ?>
+                    <a href="write.php">WRITE</a>
+                    <a href="logout.php">LOGOUT</a>
+                <?php } else { ?>
+                    <a href='login.php'>LOGIN</a>
+                <?php } ?>
             </div>
         </header>
-
         <main>
             <h1>LIST</h1>
+            <table>
+                <tbody>
+                    <thead>
+                        <tr>
+                            <th>Nr.</th>
+                            <th>Title</th>
+                            <th>Name</th>
+                            <th>Date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <?php foreach($list as $item) { ?>
+                        <tr>
+                            <td><?=$item["i_board"]?></td>
+                            <td><td><a href="detail.php?i_board=<?=$item["i_board"]?>"><?=$item["title"]?></a></td>
+                            <td><?=$item["nm"]?></td>
+                            <td><?=$item["created_at"]?></td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+            <div>
+                <?php
+                    for($i=1; $i<=$paging_count; $i++)
+                    { ?>
+                    <span><a href="list.php?page=<?=$i?>"><?=$i?></a></span>
+                <?php } ?>
+            </div>
         </main>
     </div>
 </body>
 </html>
-    <!--
-    <.?php
-        if($nm !== "") //if(isset($_SESSION["login_user"]))
-        {
-            echo "<div>Welcome, ${nm}</div>";
-            //<div>Welcome,<.?=$nm?.>.</div> (물음표 뒤에 점 빼야함 일부러 붙인 거임)
-        }
-    ?>
-    -->
-    
-    
