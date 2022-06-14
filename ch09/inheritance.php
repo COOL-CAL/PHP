8eda83bf5318bf8b24ce6e424e373215a39c134c
COOL-CAL <78495826+COOL-CAL@users.noreply.github.com>
2022-06-14 18:02:16 +0900

    Add files via upload

--- ch09/inheritance.php
+++ ch09/inheritance.php
@@ -12,18 +12,26 @@
     //extends: 상속 키워드 (just 참조, 가지는 건 아님)
     //위로 갈 수록 추상적, 내려갈 수록 상세해짐
     //ex) 동물 <- 포유류 <- 고양이과 <- 사자 / 호랑이
+
+    //over_riding: 상속 받은 값에 덮어쓰기
     class Student extends People { //student가 people을 참조함
         private $studentId;
 
         function __construct($name, $age, $studentId) {
+            print "I'm a Student. <br>";
             $this->name = $name;
             $this->age = $age;
             $this->studentId = $studentId;
         }
         function printStudent() {
-            print "- Student - <br>";
-            $this->printPeople();
+            print "<br> - Student - <br>";
+            $this->printPeople(); //오버라이딩 했다면 28, 29번 줄 결과값 달라짐
+            // parent::printPeople();
             print "Id : " . $this->studentId . "<br>";
+        }
+
+        function printPeople() {
+            print "Student에 있는 print People<br>";
         }
     }
 
