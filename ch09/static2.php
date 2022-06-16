81cd33325e84e701ef1445be94d5eb58f8917643
COOL-CAL <grin0330@gmail.com>
2022-06-16 12:08:05 +0900

    static

--- ch09/static2.php
+++ ch09/static2.php
@@ -4,7 +4,7 @@
         public $price;
 
         function setBrand() {
-            
+
         }
         
         function myPrint() {
@@ -14,7 +14,7 @@
         static function myStaticPrint() { //static: 메모리에 올라가야 쓸 수 있음, but price는 not static. -> error.
             //파라미터로 들어온 값들만 사용하면 static이 훨씬 나음
             //객체마다 다른 값을 가졌으면 할 때는 사용 하면 안됨.
-            print "The brand of Computer is " . self::$brand . ", " . "the Price is " . $this->price . "\\. <br>";
+            print "The brand of Computer is " . self::$brand . ", " . "the Price is " . "\\. <br>";
         }
     }
 
@@ -25,6 +25,10 @@
     $c2 = new Computer;
     $c2->price = 20000;
 
+    $c->myPrint();
+    $c2->myPrint();
+
+    Computer::setBrand("LG");
     $c->myPrint();
     $c2->myPrint();
 
