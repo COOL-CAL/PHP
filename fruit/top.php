b3732db1ec149b2178244e9b5f274e862d2c4961
COOL-CAL <grin0330@gmail.com>
2022-06-17 11:43:49 +0900

    shoppingmall

---
+++ fruit/top.php
@@ -0,0 +1,22 @@
+<a href="product_list.php">Show Shops</a>
+<a href="cart.php">Cart</a>
+<?php
+    session_start();
+    if(isset($_SESSION["login_user"])) { //if logged
+        $login_user = $_SESSION["login_user"];
+
+        if($login_user[0] == "admin") { //if manager
+            echo ("
+                <a href='add_goods.php'>Product submit</a>
+                <a href='admin_buy.php'>Manage purchase</a>
+                <a href='admin_mem_list.php'>Customer list</a>
+                <a href='admin_mem_update.php'>Customer info edit</a>
+            ");
+        } else { //if customer
+            echo("
+                <a href='my_buy.php'>Manage purchase</a>
+                <a href='update_member.php'>My info edit</a>
+            ");
+        }
+        echo ("<a href='logout.php'>Logout</a>");
+    }
